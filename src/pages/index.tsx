ee3f4460b0efc0f5ac89da47829b19aa0e3e5142
Mallard_Duck <emily.m@quotehound.com>
2024-10-17 15:28:58 -0400

    Add in mocked getStaticProps() page

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+// Mocked up page that I would do for something with sanity data
+// Just wanted an example page to show how you would pass in the sanity data
+// with the getStaticProps function
+
+// I'd probably set up some base level template components to use for the pages as well :)
+
+// import { urlFor } from 'lib/api';
+
+// export default function Page({ data }) {
+//   const { title, coverImage, body } = data;
+
+//   return (
+//     <article>
+//       <h1>{title}</h1>
+//       <img src={urlFor(coverImage)} />
+
+//       <PortableText blocks={body} />
+//     </article>
+//   );
+// }
+
+// export async function getStaticProps() {
+//    Get data from Sanity
+// }
